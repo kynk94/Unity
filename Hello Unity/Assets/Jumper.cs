b0e869cac99bdd8f5a8aa3750d05620162d7cc2e
Kail9974 <41245985+kynk94@users.noreply.github.com>
2019-04-18 15:57:49 +0900

    Hello Unity

---
+++ Hello Unity/Assets/Jumper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody myRigidbody;
+
+    void Start()
+    {
+        myRigidbody.AddForce(0, 500, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
